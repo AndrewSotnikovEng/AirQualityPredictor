73bd74774ab06d2d8bde5813905424040bf8b59b
Andrii Sotnikov <andrew.sotnikov@zoho.com>
2024-05-19 09:10:48 +0300

    Implemented connection check and backup data xlsx files.

--- GetAiqData.py
+++ GetAiqData.py
@@ -1,6 +1,10 @@
+import shutil
+
 from Services.WeatherService import WeatherService
 from Services.LocationService import LocationService
 from Services.ExcelService import ExcelService
+import requests
+import sys
 
 #Get current location example
 '''url =  LocationService.get_aqi_url()
@@ -9,8 +13,32 @@ aiq = WeatherService.take_data_by_url(url)
 print(WeatherService.parse_air_quality_data(aiq) )
 '''
 
-ExcelService.create_file()
+# check connection
+try:
+    response = requests.get("http://www.google.com", timeout=5)
+    if response.status_code == 200:
+        print("Connected to the internet")
+    else:
+        print("Failed to connect to the internet")
+        sys.exit(0)
+except requests.ConnectionError:
+    print("Failed to connect to the internet")
+    sys.exit(0)
+
+# do backup
+source_path = 'data.xlsx'
+destination_path = 'data.xlsx.backup'
+
+try:
+    shutil.copyfile(source_path, destination_path)
+    print("Backup created successfully.")
+except IOError as e:
+    print("Unable to copy file. %s" % e)
+except:
+    print("Unexpected error:", sys.exc_info())
+
 #process all data
+ExcelService.create_file()
 locations = LocationService.get_location_list()
 
 for location in locations:
